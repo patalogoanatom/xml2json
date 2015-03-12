c28d8ae828cfef3c7539f12aabe6365259c9eab3
Madiyar <madiyar.kaskabayev@cs.sdu.edu.kz>
2015-03-12 18:09:34 +0600

    59% of coverage

--- test/test_xml2json.py
+++ test/test_xml2json.py
@@ -7,6 +7,10 @@ import os
 xmlstring = ""
 options = None
 
+class A:
+    pretty = True
+
+
 class SimplisticTest(unittest.TestCase):
 
     def setUp(self):
@@ -42,6 +46,52 @@ class SimplisticTest(unittest.TestCase):
         #print json_data["root"]["table"][0]["tr"]
         self.assertTrue("table" in json_data["root"])
         self.assertEqual(json_data["root"]["table"][0]["tr"]["td"] , ["Apples", "Bananas"])
+
+    def test_json2xml(self):
+    	test_case = '{"note": {"to": " Tove","from": "Jani","heading": "Reminder","body": "Dont forget me this weekend!"}}'
+    	result = xml2json.json2xml(test_case)
+    	self.assertEqual(result , '<note><body>Dont forget me this weekend!</body><to> Tove</to><from>Jani</from><heading>Reminder</heading></note>')
+
+    def test_xml2json(self):
+        test_case = "   <root><hi>Hello</hi><text>1</text><text>2</text><text>3</text><text>4</text><num>3</num><Sulya>1</Sulya><Sulya>2</Sulya><Sulya>3</Sulya><Sulya>4</Sulya><list><a>1</a><b>2</b><c>3</c></list></root>"
+        result = xml2json.xml2json(test_case, A());
+        self.assertEqual(result, '{\n    "root": {\n        "Sulya": [\n            "1",\n            "2",\n            "3",\n            "4"\n        ],\n        "hi": "Hello",\n        "list": {\n            "a": "1",\n            "b": "2",\n            "c": "3"\n        },\n        "num": "3",\n        "text": [\n            "1",\n            "2",\n            "3",\n            "4"\n        ]\n    }\n}')
+
+    def test_json2elem(self):
+        test_case = '''{
+                        "root" : {
+                            "@":"Hello",
+                            "#text":[1,2,3,4], 
+                            "#tail":3,
+                            "Sulya":[1,2,3,4], 
+                            "list":{
+                                        "a":1, 
+                                        "b":2, 
+                                        "c":3
+                                    }
+                        }
+                    }'''
+
+        test_except = '''{
+                "root" : {
+                    "@":"Hello",
+                    "#text":[1,2,3,4], 
+                    "#tail":3,
+                    "Sulya":[1,2,3,4], 
+                    "list":{
+                                "a":1, 
+                                "b":2, 
+                                "c":3
+                            }
+                },
+                "aasdf" : "Fail"
+            }'''
+        try:
+            result = xml2json.json2elem(test_except)
+        except Exception, e:
+            self.assertTrue(isinstance(e, ValueError))    
+
+        result = xml2json.json2elem(test_case)
 
 if __name__ == '__main__':
     unittest.main()
